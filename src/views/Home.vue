5fc27496e169fd6fb8815a3b1f93f018b46222ea
djurnamn <bjorn@pocketsize.se>
2020-09-09 08:32:02 +0200

    conditional login message on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,23 @@
 
 			<p>Welcome to the Simple Dependency Scanner, an app prototype built to communicate with the Debricked vulnerability scan api.</p>
 
-			<p>To get started, please <router-link to="/login" class="navigation-link">log in</router-link>!</p>
+			<p v-if="!loggedIn">To get started, please <router-link to="/login" class="navigation-link">log in</router-link>!</p>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'Home'
+	name: 'Home',
+
+	computed: {
+		/**
+		 * Check if user is logged in
+		 */
+		loggedIn() {
+			return this.$store.state.auth.status.loggedIn
+		}
+	}
 }
 </script>
 
